fc514b89c8db410c249bc44faf8a5f2b54cecd5a
FKHals <5229803-FKHals@users.noreply.gitlab.com>
2021-02-11 13:47:14 +0100

    Revert patch changes to stf/build.gradle.kts

--- stf/build.gradle.kts
+++ stf/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
   id("saros.gradle.eclipse.plugin")
-  `java-library`
 }
 
 val versionQualifier = (ext.get("versionQualifier") ?: "") as String
@@ -25,19 +24,18 @@ configurations {
 
 dependencies {
     if (junitVersion != null) {
-        implementation(junitVersion)
+        compile(junitVersion)
     }
-    implementation(project(":saros.core"))
-    implementation(project(":saros.eclipse"))
+    compile(project(":saros.core"))
+    compile(project(":saros.eclipse"))
     // This is a workaround for https://github.com/saros-project/saros/issues/1086
     implementation("org.eclipse.platform:org.eclipse.urischeme:1.1.0")
     // This is a workaround for https://github.com/saros-project/saros/issues/1114
     implementation("org.eclipse.platform:org.eclipse.ui.ide:3.17.200")
     implementation("org.eclipse.platform:org.eclipse.ui.workbench:3.120.0")
-    implementation(project(path = ":saros.eclipse", configuration = "testing"))
+    compile(project(path = ":saros.eclipse", configuration = "testing"))
 
     releaseDep(fileTree("libs"))
-    api(fileTree("libs"))
 }
 
 sourceSets {
