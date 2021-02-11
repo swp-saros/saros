0af2e10dcd14d1fd1aaa9ca7ed94f6cf014cc0a0
Eike Taegener <e.taegener@t-online.de>
2021-02-11 13:09:07 +0100

    releaseDep usees fileTree in build.gradle of stf.

--- stf/build.gradle.kts
+++ stf/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     implementation("org.eclipse.platform:org.eclipse.ui.workbench:3.120.0")
     implementation(project(path = ":saros.eclipse", configuration = "testing"))
 
-    //releaseDep(fileTree("libs"))
+    releaseDep(fileTree("libs"))
     api(fileTree("libs"))
 }
 
